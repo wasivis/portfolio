b52ec6b3b484313f35acbcc48a14c757a10ecf1c
Wasivis <ivanmontequin@gmail.com>
2024-02-03 21:46:33 -0300

    Update components to remove warnings.

--- src/components/Tags/index.js
+++ src/components/Tags/index.js
@@ -1,25 +1,28 @@
-import React, { useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
-import "./index.scss";
+import React, { useEffect, useRef } from "react"
+import { useLocation } from "react-router-dom"
+import "./index.scss"
 
 const Tags = () => {
-  const topTagsRef = useRef(null);
-  const bottomTagsRef = useRef(null);
-  const location = useLocation();
+  const topTagsRef = useRef(null)
+  const bottomTagsRef = useRef(null)
+  const location = useLocation()
 
   useEffect(() => {
-    const animateTags = () => {
-      topTagsRef.current?.classList.add("animate-tags");
-      bottomTagsRef.current?.classList.add("animate-tags");
-    };
+    const topTagsCurrent = topTagsRef.current
+    const bottomTagsCurrent = bottomTagsRef.current
 
-    animateTags();
+    const animateTags = () => {
+      topTagsCurrent?.classList.add("animate-tags")
+      bottomTagsCurrent?.classList.add("animate-tags")
+    }
+
+    animateTags()
 
     return () => {
-      topTagsRef.current?.classList.remove("animate-tags");
-      bottomTagsRef.current?.classList.remove("animate-tags");
-    };
-  }, [location]);
+      topTagsCurrent?.classList.remove("animate-tags")
+      bottomTagsCurrent?.classList.remove("animate-tags")
+    }
+  }, [location])
 
   return (
     <>
@@ -34,7 +37,7 @@ const Tags = () => {
         <span className="tags bottom-html">{"</html>"}</span>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Tags;
+export default Tags
