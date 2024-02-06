1ba7780c893839fec847fa962388a938ec155297
Wasivis <ivanmontequin@gmail.com>
2024-02-05 22:56:03 -0300

    Fix warnings for deployment.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react"
+import React, { useState, useRef } from "react"
 import AnimatedLetters from "../AnimatedLetters"
 import "./index.scss"
 import projectsData from "../../data/projects.json"
